4bbd55adbf21de9fc363f632fe68c3ae0e450374
Hardik Prajapati <prajapatihardik375@gmail.com>
2026-02-09 17:15:49 +0530

    normal

--- Studio97/src/Constants/Videopage/Video.jsx
+++ Studio97/src/Constants/Videopage/Video.jsx
@@ -5,7 +5,7 @@ const Video = () => {
   return (
     <>
     <div >
-        <video src='/Videos/videoplayback.mp4' className=' h-screen w-screen border-none relative object-cover ' muted autoPlay loop >
+        <video src='/Videos/videoplayback.mp4' className=' h-screen w-screen border-none relative object-cover ' muted autoPlay loop playsInline >
         </video>
 
     </div>
