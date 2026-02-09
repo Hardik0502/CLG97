f3b97bb27d7bd83ba0a407f9361d4e6e85b786ad
Hardik Prajapati <prajapatihardik375@gmail.com>
2026-02-09 17:51:27 +0530

    Remove the video and uploaded from cloudinary.com

--- Studio97/src/Constants/Videopage/Video.jsx
+++ Studio97/src/Constants/Videopage/Video.jsx
@@ -5,7 +5,7 @@ const Video = () => {
   return (
     <>
     <div >
-        <video src='/Videos/videoplayback.mp4' className=' h-screen w-screen border-none relative object-cover ' muted autoPlay loop playsInline >
+        <video src='https://res.cloudinary.com/ddhh0a3an/video/upload/v1770639319/videoplayback_nt00tk.mp4' className=' h-screen w-screen border-none relative object-cover ' muted autoPlay loop playsInline >
         </video>
 
     </div>
